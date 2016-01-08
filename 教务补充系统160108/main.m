76ac20fc261360a0fe2ec813c97ebb44ced64a0f
qianfeng <qianfeng@teacherUU.local>
2016-01-08 17:03:05 +0800

    Initial Commit

---
+++ 教务补充系统160108/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  教务补充系统160108
+//
+//  Created by qianfeng on 16/1/8.
+//  Copyright (c) 2016年 qianfeng. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
